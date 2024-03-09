10d2f76ed365f3ce66117270c910e2da81dace10
anatasija4572 <74258980+anatasija4572@users.noreply.github.com>
2024-03-09 15:17:33 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Task 2
+//Make a new console application called StudentGroup
+//Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+//Get a number from the console between 1 and 2 and print the students from that group in the console.
+//Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+//Test Data:
+//Enter student group: (there are 1 and 2 )
+//1
+//Expected Output:
+//The Students in G1 are:
+//Zdravko
+//Petko
+//Stanko
+//Branko
+//Trajko
+
+string[] studentsG1 = new string[] { "Sage", "Jett", "Phoenix", "Reyna", "Yoru" };
+string[] studentsG2 = new string[] { "Harbor", "Astra", "Deadlock", "Gekko" };
+
+Console.WriteLine("Would you like to vew student group 1 or 2?");
+
+int i = int.Parse(Console.ReadLine());
+
+foreach (string student in studentsG1)
+{
+    if (i == 1)
+        continue;
+    Console.WriteLine(student + " ");
+}
+
+foreach (string student in studentsG2)
+{
+    if (i == 2)
+        continue;
+    Console.WriteLine(student + " ");
+}
---
+++ Homework_01/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Make a console application called SumOfEven. Inside it create an array of 6 integers. Get numbers from the input, find and print the sum of the even numbers from the array:
+
+//using System.Diagnostics.Metrics;
+
+//Test Data:
+//Enter integer no.1:
+//4
+//Enter integer no.1:
+//3
+//Enter integer no.1:
+//7
+//Enter integer no.1:
+//3
+//Enter integer no.1:
+//2
+//Enter integer no.1:
+//8
+//Expected Output:
+//The result is: 14
+Console.WriteLine("--------->Find sum of all elements in an array<---------");
+
+Console.WriteLine("Enter 6 integers to fill the array!");
+
+int[] numberArry = new int[6];
+int i, sum = 0;
+
+for (i = 0; i < 6; i++)
+{
+    Console.WriteLine("\n Enter integer no.{0}:", i);
+    numberArry[i] = int.Parse(Console.ReadLine());
+}
+
+for (i = 0; i < 6; i++)
+{
+    if (numberArry[i] % 2 == 0)
+    {
+        sum += numberArry[i];
+    }
+    else
+    {
+        continue;
+    }
+}
+
+Console.WriteLine("Sum of all the  even emelents in the array is: {0}", sum);
